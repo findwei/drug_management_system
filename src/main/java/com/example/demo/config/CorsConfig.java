fe2492518589ab2462ab2e8171bc4c0bc88108b5
panghu <1214740274@qq.com>
2020-04-03 09:02:35 +0800

    解决跨域

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@SpringBootConfiguration
+public class CorsConfig implements WebMvcConfigurer {
+    static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS).allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
